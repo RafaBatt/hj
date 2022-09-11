9a131fd29d789e8a3ed04f6663a69f4314bedfdb
RafaBatt <113286995+RafaBatt@users.noreply.github.com>
2022-09-11 15:25:46 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $('.bike-slider').slick({
+        arrows: false,
+        dots: true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed: 1800,
+    });
+    
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed: 1800,
+    });
+
+});
